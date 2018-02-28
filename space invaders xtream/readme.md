fca5fba66c14d9c4f3dfa35845d036a27720ab13
fanbyprinciple <fanbyprinciple@gmail.com>
2018-02-28 20:29:06 +0530

    added enemy explosion using canvas and readme updated

--- space invaders xtream/readme.md
+++ space invaders xtream/readme.md
@@ -14,18 +14,18 @@ Note. if player is hit by enemy its instant game over.
 functions - 
 `function draw_shield()` //handles the creation of shield and if its low then turns into red color
 
-`for (var enemy in enemies) {
-        if(collides(enemies[enemy],shield)) {
-            shieldHealth --;` //handles reducing shield health when hit 
+`for (var enemy in enemies) {`
+`        if(collides(enemies[enemy],shield)) {`
+`            shieldHealth --;` //handles reducing shield health when hit` 
 
 
 ###### B. New Enemy type
 
 The new enemy type is created by photoshopping the normal enemy sprite and modifying the `enemy` object. A new attribute weight is introduced in `enemy` object which randomly assigns weight as either 10 or 20.  `I.weight = 10 + (Math.floor(Math.random() * 2) )* 10;` and then this weight is used to modify the sprite that appears in game by simple if else.
-`if(I.weight == 10)
-        I.sprite = Sprite("enemy");
-    else
-        I.sprite = Sprite("enemyMod");`
+`if(I.weight == 10)`
+`        I.sprite = Sprite("enemy");`
+`    else`
+`        I.sprite = Sprite("enemyMod");`
 
 The new enemy instead of following a zig zag pattern comes right down and are difficult to deal with.
 
@@ -36,20 +36,20 @@ Score is updated whenever enemy explodes and `enemy.explode` function is called 
 `score += I.weight;` 
 The score is displayed by `function draw_score()`
 This scoring function is then used to increase the level after every 500 points.
-`function draw_level(){
-    ctx.font = "16px monospace";
-    ctx.fillStyle = "#0095DD";
-    level = (score - (score%500))/500;
-    ctx.fillText("Level: " + level, CANVAS_WIDTH/2-40, 20 )
-}`
+`function draw_level(){`
+`    ctx.font = "16px monospace";`
+`    ctx.fillStyle = "#0095DD";`
+`    level = (score - (score%500))/500;`
+`    ctx.fillText("Level: " + level, CANVAS_WIDTH/2-40, 20 )`
+`}`
 
 ###### E. Enemy System
 
 Apart from the introduction of new enemy following changes have been made-
 
 1. Now the enemy creation function has been modified 
-` if(Math.random() < 0.02 + 0.05 * level){
-        enemies.push(Enemy());`  
+` if(Math.random() < 0.02 + 0.05 * level){`
+`        enemies.push(Enemy());`  
    
 so as the level increase more enemies are created. 
 
@@ -59,19 +59,19 @@ so as the level increase more enemies are created.
 ###### F. New object shield 
 
 A new object shield is created.
-`var shield = {
-    ...
-    draw: function(){ 
-        ...
-    } ,
-    explode: function(){
-        ...
-        this.color = "red"
-        setTimeout(()=>{
-            this.color = "#222239"}, 500
-        );
-    }
-}`
+`var shield = {`
+`    ...`
+`    draw: function(){` 
+`        ...`
+`    } ,`
+`    explode: function(){`
+`        ...`
+`        this.color = "red"`
+`        setTimeout(()=>{`
+`            this.color = "#222239"}, 500`
+`        );`
+`    }`
+`}`
 
 When shield comes to contact with enemy, it reduces the shield health. So `collisiondetection()` is applied for this too.
 
@@ -92,14 +92,20 @@ THis alert box displays the score and the level.
 
 ###### I. Explosion sprite
 
-whenever the enemy or the player is hit, the sprite is dynamically changed to explosion.
+whenever the the player is hit, the sprite is dynamically changed to explosion.
 `I.explode = function() {
-        ...
-        Sound.play("explosion");
-        this.sprite = Sprite("explosion");
-        setTimeout (()=>{this.active = false},100);`
+`        ...`
+`        Sound.play("explosion");`
+`        this.sprite = Sprite("explosion");`
+`        setTimeout (()=>{this.active = false},100);`
 
-setTimeout is used so that the enemy explosion sprite is shown for atleast 100ms. Same for player sprite.
+setTimeout is used so that the player explosion sprite is shown for atleast 100ms. 
+
+for enemy explosion this game uses 'canvas.drawimage' function.
+`var img = new Image();`
+`img.src = "images/explosion.png";`
+`img.addEventListener("load", function drawPic(x, y){ctx.drawImage(img, x, y)}, false);`
+
 
 ###### J. When shield is hit it turns red and after 3 hits another hit is game over.
 ###### K. The shield also turns red and progressively empty as more enemy hit it.
--- space invaders xtream/scripts/game.js
+++ space invaders xtream/scripts/game.js
@@ -3,58 +3,49 @@ var CANVAS_HEIGHT = 360
 var level = 1;
 var score = 0;
 var shieldHealth = 3;
+// lives --used to denote how many enemies can actually hit the shield before gameover
+var ENEMY_SPAWN_RATE = 0.02;
 
-var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "' ><script>console.log('canvas created')</script></canvas>"); 
-//creates a canvas element in the document body
-
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "' ><script>console.log('canvas created')</script></canvas>");
+//dynamically creates a canvas element in the document body
 
 var ctx = canvasElement.get(0).getContext("2d");
 // ctx short for canvas context
 
 canvasElement.appendTo('body');
 
-var FPS = 30;   //frames per second
-var ExplosionInterval = 10;
-setInterval (()=>{
+var FPS = 30; //frames per second
+setInterval(() => {
     update();
     draw();
-}, 1000/FPS);
+}, 1000 / FPS);
 // handles the frame
 
-
-//var blanks = true; 
-// blanks are used to create a dual color laser so that the bullets don't look continuous
-//var blankCtr = 2; // blanks fired every 2nd iteration
-
-//var enemySpawnRate =5;
+var img = new Image();
+img.src = "images/explosion.png";
+img.addEventListener("load", function drawPic(x, y){ctx.drawImage(img, x, y)}, false);
 
 var playerBullets = [];
+
 function Bullet(I) {
 
     I.active = true;
     I.xVelocity = 0;
     I.yVelocity = -I.speed;
-    I.width = 4 ;
+    I.width = 4;
     I.height = 7;
     I.color = "white";
 
-    I.inBounds = function(){
+    I.inBounds = function () {
         return I.x >= 0 && I.x <= CANVAS_WIDTH && I.y >= 0 && I.y <= CANVAS_HEIGHT;
     }
 
-    I.draw = function(){
-        // if(!I.blanks){    
-        //     ctx.fillStyle = this.color;
-        // }
-        // else {
-        //     I.active = false;
-        //     ctx.fillStyle = "white"; 
-        // }
+    I.draw = function () {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height); 
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 
-    I.update = function() {
+    I.update = function () {
         I.x += I.xVelocity;
         I.y += I.yVelocity;
         I.active = I.active && I.inBounds();
@@ -64,51 +55,57 @@ function Bullet(I) {
 } // handles bullets
 
 enemies = [];
+
 function Enemy(I) {
     I = I || {};
 
     I.active = true;
-    I.age= Math.floor(Math.random()* 128);
+    I.age = Math.floor(Math.random() * 128);
     I.color = "#A2B";
-    I.weight = 10 + (Math.floor(Math.random() * 2) )* 10;
+    I.weight = 10 + (Math.floor(Math.random() * 2)) * 10;
+    // I.weight randomly either assigns weight 10 or 20 to the enemy.
 
-    I.x = CANVAS_WIDTH / 4 + Math.random() * CANVAS_WIDTH/2;
+    I.x = CANVAS_WIDTH / 4 + Math.random() * CANVAS_WIDTH / 2;
     I.y = 0;
     I.xVelocity = 0;
     I.yVelocity = 1 + level; // velocity increases with level
     I.width = 32;
     I.height = 32;
 
-    I.inBounds = function() {
-        return I.x >=0 && I.x <= CANVAS_WIDTH && I.y >= 0 && I.y <= CANVAS_HEIGHT;
+    I.inBounds = function () {
+        return I.x >= 0 && I.x <= CANVAS_WIDTH && I.y >= 0 && I.y <= CANVAS_HEIGHT;
     };
-    console.log(I.weight);
-    if(I.weight == 10)
+    
+    if (I.weight == 10)
         I.sprite = Sprite("enemy");
     else
         I.sprite = Sprite("enemyMod");
 
-    I.explode = function() {
-        
+    I.explode = function () {
+
         score += I.weight;
-        
+
         console.log("Score :" + score);
         Sound.play("explosion");
-        this.sprite = Sprite("explosion");
-        setTimeout (()=>{this.active = false},100);
+        this.active = false;
+        ctx.drawImage(img,this.x,this.y);
+        // this.sprite = Sprite("explosion");
+        // setTimeout(() => {
+        //     this.active = false
+        // }, 100);
         //extra credits for explosion graphics
     }
 
-    I.draw = function() {
+    I.draw = function () {
         this.sprite.draw(ctx, this.x, this.y)
     }
 
-    I.update = function() {
+    I.update = function () {
         I.x += I.xVelocity;
         I.y += I.yVelocity;
-        if(I.weight == 10){
-            I.xVelocity = 3 * Math.sin(I.age * Math.PI /64);}
-        else {
+        if (I.weight == 10) {
+            I.xVelocity = 3 * Math.sin(I.age * Math.PI / 64);
+        } else {
             I.xVelocity = 0;
             I.yVelocity = 2 + level;
         }
@@ -124,8 +121,8 @@ var player = {
     y: 270,
     width: 32,
     height: 32,
-    sprite: Sprite("player") ,
-    draw: function(){
+    sprite: Sprite("player"),
+    draw: function () {
         this.sprite.draw(ctx, this.x, this.y)
     }
 }; // handles player creation
@@ -133,34 +130,34 @@ var player = {
 var shield = {
     color: "#222239",
     x: 0,
-    y: CANVAS_HEIGHT-24,
+    y: CANVAS_HEIGHT - 24,
     width: CANVAS_WIDTH,
     height: 30,
-    draw: function(){
-        
+    draw: function () {
+
         ctx.fillStyle = this.color;
-        ctx.fillRect( this.x, this.y, this.width, this.height);
-    } ,
-    explode: function(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+    explode: function () {
         console.log("shield hit");
         this.color = "red"
-        setTimeout(()=>{
-            this.color = "#222239"}, 500
-        );
+        setTimeout(() => {
+            this.color = "#222239"
+        }, 500);
     }
-}
+} // handles shield
 
 
-function collides (a,b) {
-    return (a.x < b.x + b.width && a.x +a.width > b.x && a.y < b.y +b.height && a.y +a.height > b.y);
+function collides(a, b) {
+    return (a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y);
 }
 
 
 function handleCollisions() {
-    
-    for (var bullet in playerBullets){
-        for (var enemy in enemies){
-            if (collides(playerBullets[bullet],enemies[enemy])){
+
+    for (var bullet in playerBullets) {
+        for (var enemy in enemies) {
+            if (collides(playerBullets[bullet], enemies[enemy])) {
                 console.log("enemy hit")
                 playerBullets[bullet].active = false;
                 enemies[enemy].explode();
@@ -168,9 +165,9 @@ function handleCollisions() {
             }
         }
     }
-    
+
     for (var enemy in enemies) {
-        if(collides(enemies[enemy],player)) {
+        if (collides(enemies[enemy], player)) {
             player.explode();
             enemies[enemy].explode();
             break;
@@ -178,155 +175,148 @@ function handleCollisions() {
     }
 
     for (var enemy in enemies) {
-        if(collides(enemies[enemy],shield)) {
-            shieldHealth --;
-            
+        if (collides(enemies[enemy], shield)) {
+            shieldHealth--;
+
             shield.explode();
             enemies[enemy].explode();
             break;
         }
     }
-      
+
 
 }
 
-player.shoot =  function(){
-    //console.log("beep beep");
+player.shoot = function () {
+    
     Sound.play("shoot");
-    // blankCtr = blankCtr - 1;
-    // if(blankCtr == 0){
-    //     blankCtr = 2 ;
-    //     blanks = !(blanks);
-    // }
 
-    player.midpoint = function(){
+    player.midpoint = function () {
         return {
-            x: this.x + this.width/2 - 1,
-            y: this.y + this.height/2
+            x: this.x + this.width / 2 - 1,
+            y: this.y + this.height / 2
         };
-     };
+    };
 
 
     var bulletPosition = this.midpoint();
 
     playerBullets.push(Bullet({
         speed: 5,
-        //blanks: blanks,
         x: bulletPosition.x,
         y: bulletPosition.y
     }))
-    
+
 }
 
-player.explode = function() {
+player.explode = function () {
     console.log("player hit");
     Sound.play("explosion");
     this.sprite = Sprite("explosion");
-    setTimeout(()=>{
+    setTimeout(() => {
         alert("Mission Control : your spaceship is hit. \n\n We salute your martyrdom in scoring \t" + score + ",\n and defending till level \t" + level + ".\n\n Play again ?");
-        document.location.reload();                                        
-    }, 200); 
-     
+        document.location.reload();
+    }, 200);
+
     this.active = false;
 }
 
-function update(){
+function update() {
 
-    if(keydown.space) {          
-            player.shoot();
+    if (keydown.space) {
+        player.shoot();
     }
 
 
 
-    if(keydown.left) {
+    if (keydown.left) {
         player.x += -4
     }
 
-    if(keydown.right) {
+    if (keydown.right) {
         player.x += +4
     }
 
-    player.x = player.x.clamp(0, CANVAS_WIDTH - player.width) 
+    player.x = player.x.clamp(0, CANVAS_WIDTH - player.width)
     //restricts player from going off-screen (x -axis)
-    
-    playerBullets.forEach(function(bullet) {
-        bullet.update() ;
+
+    playerBullets.forEach(function (bullet) {
+        bullet.update();
     })
 
-    playerBullets = playerBullets.filter(function(bullet) {
+    playerBullets = playerBullets.filter(function (bullet) {
         return bullet.active;
     })
 
-    enemies.forEach(function(enemy) {
-        enemy.update() ;
+    enemies.forEach(function (enemy) {
+        enemy.update();
     });
 
-    enemies = enemies.filter (function(enemy){
+    enemies = enemies.filter(function (enemy) {
         return enemy.active;
     })
 
-    if(Math.random() < 0.02 + 0.05 * level){
+    if (Math.random() < ENEMY_SPAWN_RATE + 0.05 * level) {
         enemies.push(Enemy());
     }
 
-    
+
 };
 
-function draw_score(){
+function draw_score() {
     ctx.font = "16px monospace";
     ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+ score, 8, 20);
+    ctx.fillText("Score: " + score, 8, 20);
 }
 
-function draw_shield(){
+function draw_shield() {
     ctx.font = "16px monospace";
-    if (shieldHealth*5 + 45 < 30){
+    if (shieldHealth * 16 < 30) {
         ctx.fillStyle = "red";
-    
-    }
-    else {
+
+    } else {
         ctx.fillStyle = "#0095DD";
     }
-    ctx.fillText("Shield:" , CANVAS_WIDTH-80, 20)
-    ctx.fillRect(CANVAS_WIDTH-80,30,shieldHealth*5 + 45 ,20)
-    if ((shieldHealth*5+ 45) < 0) {
-        
+    ctx.fillText("Shield:", CANVAS_WIDTH - 80, 20)
+    ctx.fillRect(CANVAS_WIDTH - 75, 30, shieldHealth * 16, 20)
+    if ((shieldHealth * 16) < 0) {
+
         alert("Mission Control : The Enemy has landed. Base is lost. \n\n You scored \t" + score + ",\n and defended till level \t" + level + ".\n\n Play again ?");
-        document.location.reload();                                        
+        document.location.reload();
     }
 
 }
 
-function draw_level(){
+function draw_level() {
     ctx.font = "16px monospace";
     ctx.fillStyle = "#0095DD";
-    level = (score - (score%500))/500;
-    ctx.fillText("Level: " + level, CANVAS_WIDTH/2-40, 20 )
+    level = (score - (score % 200)) / 200;
+    ctx.fillText("Level: " + level, CANVAS_WIDTH / 2 - 40, 20)
 }
 
 
 
-function draw(){
+function draw() {
     //console.log("draw called")
-    
-    ctx.clearRect( 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    var grd=ctx.createLinearGradient(0,0,0,1 + 120 * level);
-    //var grd=ctx.createRadialGradient(90,50,5,90,60,200);
-    grd.addColorStop(0,"#000000");
-    grd.addColorStop(1,"#53346D");
 
-    ctx.fillStyle=grd;
-    ctx.fillRect( 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-   
-    
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    var grd = ctx.createLinearGradient(0, 0, 0, 1 + 120 * level);
+    //var grd=ctx.createRadialGradient(90,50,5,90,60,200);
+    grd.addColorStop(0, "#000000");
+    grd.addColorStop(1, "#53346D");
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+
     player.draw();
     shield.draw();
 
-    playerBullets.forEach(function(bullet){
+    playerBullets.forEach(function (bullet) {
         bullet.draw();
     });
 
-    enemies.forEach(function(enemy){
+    enemies.forEach(function (enemy) {
         enemy.draw();
     });
     draw_level();
@@ -334,9 +324,3 @@ function draw(){
     draw_shield();
     handleCollisions();
 }
-
-
-
-
-
-
